700eca764748ef0435d3ecfa666ac9dbb33003ab
LXH Cat <boshiyk@foxmail.com>
2026-03-16 09:10:01 +0800

    商铺详情页面缓存功能

--- src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -6,6 +6,9 @@ import com.hmdp.service.IShopTypeService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+import java.util.List;
+
 /**
  * <p>
  *  服务实现类
@@ -17,4 +20,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
 
+    @Override
+    public List<ShopType> typeList() {
+
+        return null;
+    }
 }
