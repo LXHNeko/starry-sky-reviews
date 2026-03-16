492801716e2633e257b5cf35e7db0169e4261dd0
LXH Cat <boshiyk@foxmail.com>
2026-03-16 08:34:56 +0800

    项目初始化 & 基于Redis实现登录功能

---
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -0,0 +1,35 @@
+package com.hmdp.utils;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.StrUtil;
+import com.hmdp.dto.UserDTO;
+import com.hmdp.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.LOGIN_USER_KEY;
+import static com.hmdp.utils.RedisConstants.LOGIN_USER_TTL;
+import static net.sf.jsqlparser.util.validation.metadata.NamedObject.user;
+
+@RequiredArgsConstructor
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 1.判断是否需要拦截（ThreadLocal中是否有用户）
+        if(UserHolder.getUser() == null){
+            // 没有，需要拦截，返回401
+            response.setStatus(401);
+            // 拦截
+            return false;
+        }
+        // 放行
+        return true;
+    }
+}
