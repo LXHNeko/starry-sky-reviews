bc027b788f5d4ae6507fd8acd565735ffb2ef9db
LXH Cat <boshiyk@foxmail.com>
2026-03-17 11:48:32 +0800

    在店铺模块中优化缓存功能，采用逻辑过期方案解决缓存击穿问题

---
+++ src/main/java/com/hmdp/utils/CacheClient.java
@@ -0,0 +1,25 @@
+package com.hmdp.utils;
+
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.entity.Shop;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+
+import static com.hmdp.utils.RedisConstants.CACHE_NULL_TTL;
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TTL;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CacheClient {
+
+
+
+}
--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,9 +1,13 @@
 package com.hmdp;
 
+import com.hmdp.service.IShopService;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
+
 
 
 }
